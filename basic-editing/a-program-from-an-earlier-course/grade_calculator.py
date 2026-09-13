926473861fb4f85ddb40c3017484f1bd9c37118e
kenvu2 <joshuy2014@gmail.com>
2026-09-13 18:43:30 -0400

    -created-configurations

--- basic-editing/a-program-from-an-earlier-course/grade_calculator.py
+++ basic-editing/a-program-from-an-earlier-course/grade_calculator.py
@@ -28,3 +28,35 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+class Student:
+    def __init__(self, name, student_id):
+        self.name = name
+        self.student_id = student_id
+        self.grades = []
+
+    def add_grade(self, grade):
+        self.grades.append(grade)
+
+    def get_average(self):
+        return sum(self.grades) / len(self.grades)
+
+    def get_name(self):
+        return self.name
+    def process_students():
+        student1 = Student("Alice", 1001)
+        student1.add_grade(90)
+        student1.add_grade(85)
+        student1.name = "Alice Smith"
+        print(student1.get_average())
+
+        student2 = Student("Bob", 1002)
+        student2.add_grade(70)
+        student2.add_grade(75)
+        student2.student_id = 1003
+        print(student2.get_name())
+
+        student3 = Student("Carla", 1004)
+        student3.add_grade(100)
+        rint(student3.get_average())
+    
